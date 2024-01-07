07415e58c860d2b862cec843667ebcb294ac0a9e
Mudit Agrawal <muditagarwalgkp@gmail.com>
2024-01-07 19:22:18 +0530

    ca-crud

--- routes/ca.js
+++ routes/ca.js
@@ -1,4 +1,10 @@
 import express from "express";
 const router = express.Router();
+import { createCA,getCAById,updateCA,deleteCA } from "../Controllers/ca.js";
+
+router.post("/createCA", createCA);
+router.get("/getCA", getCAById);
+router.patch("/updateCA", updateCA);
+router.delete("/deleteCA", deleteCA);
 
 export default router;
