a227092a7e78d640a555336f04e63ba99e6eb092
vipul <vipulkumar2426@gmail.com>
2024-01-07 23:06:59 +0530

    user crud

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,14 @@
 import express from "express";
+import { addEvent, checkEmail, createuser, deleteEvent, deleteUserByEmail, getUser, updateUser } from '../Controllers/user.js';
+
 const router = express.Router();
+
+router.get('/getAllUser',getUser)
+router.post('/createUser',createuser)
+router.get('/checkUser',checkEmail)
+router.patch('/updateuser',updateUser)
+router.delete('/deleteUser',deleteUserByEmail)
+router.post('/addEvent',addEvent)
+router.delete('/deleteEvent',deleteEvent)
 
 export default router;
