dec36b1461c5b5a40cb7ea95dea6e700af8f00cb
Ishika Banerjee <ishika.banerjee.cd.cse21@iitbhu.ac.in>
2024-01-24 02:00:10 +0530

    Revert "Merge pull request #16 from MuditAgrawal9/update-api" :wq
    This reverts commit f0fe30b283737ecada19b7f2f0cd43c9f7766ca1, reversing
    changes made to c44aa9214b44ae2b4d2c36586d40c7ca5c34c78e.

--- Controllers/event.js
+++ Controllers/event.js
@@ -1,174 +1,147 @@
 //for writing api function
-import { EventModel } from "../models/events.js";
+import { EventModel } from "../models/events.js"
 import { PreEventModel } from "../models/events.js";
 import { GuestTalksModel } from "../models/events.js";
-
-const getEvents = async (req, resp) => {
-  try {
-    let response = await EventModel.find();
-    resp.send({ result: response, success: true });
-  } catch (err) {
-    if (err) {
-      resp.send({ result: "Error getting events data", success: false });
+ const getEvents=async(req,resp)=>{
+   try{
+    let response=await EventModel.find();
+   resp.send({result:response,success:true})
+   }
+   catch(err){
+    if(err){
+        resp.send({result:"Error getting events data",success:false})
     }
-  }
-};
-
-const getPreEvents = async (req, resp) => {
-  try {
-    let response = await PreEventModel.find();
-    resp.send({ result: response, success: true });
-  } catch (err) {
-    if (err) {
-      resp.send({ result: "Error getting preEvents data", success: false });
+   }
+}
+const getPreEvents=async(req,resp)=>{
+    try{
+        let response=await PreEventModel.find();
+       resp.send({result:response,success:true})
+       }
+       catch(err){
+        if(err){
+            resp.send({result:"Error getting preEvents data",success:false})
+        }
+       }
+}
+const getGuestTalks=async(req,resp)=>{
+    try{
+        let response=await GuestTalksModel.find();
+       resp.send({result:response,success:true})
+       }
+       catch(err){
+        if(err){
+            resp.send({result:"Error getting guesttalks data",success:false})
+        }
+       }
+}
+const addEvent=async(req,resp)=>{
+    try{
+        let data=req.body;
+    let response=new EventModel(data);
+    response=await response.save();
+  resp.send({result:response})
     }
-  }
-};
-
-const getGuestTalks = async (req, resp) => {
-  try {
-    let response = await GuestTalksModel.find();
-    resp.send({ result: response, success: true });
-  } catch (err) {
-    if (err) {
-      resp.send({ result: "Error getting guesttalks data", success: false });
+    catch(err){
+        if(err){
+            resp.send({result:"Error saving event",success:false})
+        }
     }
-  }
-};
-
-const addEvent = async (req, resp) => {
-  try {
-    let data = req.body;
-    let response = new EventModel(data);
-    response = await response.save();
-    resp.send({ result: response });
-  } catch (err) {
-    if (err) {
-      resp.send({ result: "Error saving event", success: false });
+}
+const addPreEvent=async(req,resp)=>{
+    try{
+        let data=req.body;
+    let response=new PreEventModel(data);
+    response=await response.save();
+  resp.send({result:response})
     }
-  }
-};
-
-const addPreEvent = async (req, resp) => {
-  try {
-    let data = req.body;
-    let response = new PreEventModel(data);
-    response = await response.save();
-    resp.send({ result: response });
-  } catch (err) {
-    if (err) {
-      resp.send({ result: "Error saving preevent", success: false });
+    catch(err){
+        if(err){
+            resp.send({result:"Error saving preevent",success:false})
+        }
     }
-  }
-};
-
-const addGuestTalks = async (req, resp) => {
-  try {
-    let data = req.body;
-    let response = new GuestTalksModel(data);
-    response = await response.save();
-    resp.send({ result: response });
-  } catch (err) {
-    if (err) {
-      resp.send({ result: "Error saving guestTalks", success: false });
+}
+const addGuestTalks=async(req,resp)=>{
+    try{
+        let data=req.body;
+    let response=new GuestTalksModel(data);
+    response=await response.save();
+  resp.send({result:response})
     }
-  }
-};
-
-const updateEvent = async (req, resp) => {
-  try {
-    let { eventName, updatedBody } = req.body;
-    let response = await EventModel.updateOne(
-      { eventName: eventName },
-      {
-        $set: updatedBody,
-      }
-    );
-    resp.send({ result: response });
-  } catch (err) {
-    if (err) {
-      resp.send({ result: "Updation of Events Failed", success: false });
+    catch(err){
+        if(err){
+            resp.send({result:"Error saving guestTalks",success:false})
+        }
     }
-  }
-};
+}
+const updateEvent=async(req,resp)=>{
+   try{ let {eventName,updatedBody}=req.body;
+    let response=await EventModel.updateOne({eventName:eventName},{
+        $set:updatedBody
+    })
+    resp.send({result:response})}
+    catch(err){
+        if(err){
+            resp.send({result:"Updation of Events Failed",success:false})
 
-const updatePreEvent = async (req, resp) => {
-  try {
-    let { eventName, updatedBody } = req.body;
-    let response = await PreEventModel.updateOne(
-      { eventName: eventName },
-      {
-        $set: updatedBody,
-      }
-    );
-    resp.send({ result: response });
-  } catch (err) {
-    if (err) {
-      resp.send({ result: "Updation of PreEvent Failed", success: false });
+        }
     }
-  }
-};
+}
+const updatePreEvent=async(req,resp)=>{
+    try{let {eventName,updatedBody}=req.body;
+    let response=await PreEventModel.updateOne({eventName:eventName},{
+        $set:updatedBody
+    })
+    resp.send({result:response})}
+    catch(err){
+        if(err){
+            resp.send({result:"Updation of PreEvent Failed",success:false})
 
-const updateGuestTalks = async (req, resp) => {
-  try {
-    let { eventName, updatedBody } = req.body;
-    let response = await GuestTalksModel.updateOne(
-      { eventName: eventName },
-      {
-        $set: updatedBody,
-      }
-    );
-    resp.send({ result: response, success: true });
-  } catch (err) {
-    if (err) {
-      resp.send({ result: "Updation of GuestTalks Failed", success: false });
+        }
     }
-  }
-};
-
-const deleteEvent = async (req, resp) => {
-  try {
-    let response = await EventModel.findOneAndDelete({
-      eventName: req.body.eventName,
-    });
-    resp.send({ result: response });
-  } catch (err) {
-    if (err) {
-      resp.send({ result: "Not Able to delete Event", success: true });
+}
+const updateGuestTalks=async(req,resp)=>{
+    try{let {eventName,updatedBody}=req.body;
+    let response=await GuestTalksModel.updateOne({eventName:eventName},{
+        $set:updatedBody
+    })
+    resp.send({result:response,success:true})}
+    catch(err){
+        if(err){
+            resp.send({result:"Updation of GuestTalks Failed",success:false})
+        }
     }
-  }
-};
+}
 
-const deletepreEvent = async (req, resp) => {
-  try {
-    let response;
-  } catch (err) {
-    if (err) {
-      resp.send({ result: "Not Able to delete preEvent", success: true });
+const deleteEvent=async(req,resp)=>{
+    try{
+        let response=await EventModel.findOneAndDelete({eventName:req.body.eventName});
+        resp.send({result:response});
     }
-  }
-};
-
-const deleteguestTalks = async (req, resp) => {
-  try {
-  } catch (err) {
-    if (err) {
-      resp.send({ result: "Not Able to guestTalks Event", success: true });
+    catch(err){
+        if(err){
+            resp.send({result:"Not Able to delete Event",success:true});
+        }
     }
-  }
-};
-
-export {
-  getEvents,
-  getPreEvents,
-  getGuestTalks,
-  addEvent,
-  addGuestTalks,
-  addPreEvent,
-  updateEvent,
-  updatePreEvent,
-  updateGuestTalks,
-  deleteEvent,
-  deletepreEvent,
-  deleteguestTalks,
-};
+}
+const deletepreEvent=async(req,resp)=>{
+    try{
+        let response;
+    }
+    catch(err){
+        if(err){
+            resp.send({result:"Not Able to delete preEvent",success:true});
+        }
+    }
+}
+const deleteguestTalks=async(req,resp)=>{
+    try{
+        
+    }
+    catch(err){
+        if(err){
+            resp.send({result:"Not Able to guestTalks Event",success:true});
+        }
+    }
+}
+export {getEvents,getPreEvents,getGuestTalks,addEvent,addGuestTalks,addPreEvent,updateEvent,updatePreEvent,updateGuestTalks,deleteEvent,deletepreEvent,deleteguestTalks}
--- models/events.js
+++ models/events.js
@@ -1,40 +1,30 @@
 import mongoose from "mongoose";
 
-const eventSchema = new mongoose.Schema(
-  {
+const eventSchema = new mongoose.Schema({
     eventName: { type: String, required: true },
     link: { type: String, required: true },
     imageURL: { type: String, required: true },
     overview: { type: String, required: true },
     status: { type: Boolean, required: true },
     teamEvent: { type: Boolean, required: true },
-    contacts: [
-      {
+    contacts: [{
         name: { type: String, required: true },
-        mobile: { type: Number, required: true },
-      },
-    ],
+        mobile: { type: Number, required: true }
+    }]
+    ,
     participants: {
-      teams: [
-        {
-          type: mongoose.Types.ObjectId,
-          ref: "Teams",
-          required: true,
-        },
-      ],
-      individuals: [
-        {
-          type: mongoose.Types.ObjectId,
-          ref: "users",
-          required: true,
-        },
-      ],
-    },
-  },
-  {
+        teams: [{
+            type: mongoose.Types.ObjectId, ref: "Teams", required: true
+        }],
+        individuals: [{
+            type: mongoose.Types.ObjectId,
+            ref:"users",
+            required: true
+        }]
+    }
+}, {
     timestamps: true,
-  }
-);
+});
 
 const EventModel = mongoose.model("Event", eventSchema);
 const PreEventModel = mongoose.model("PreEvent", eventSchema);
